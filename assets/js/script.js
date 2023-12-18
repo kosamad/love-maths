e3d9aa09ed5d5caa400b3240b0396ae7c2b22ab7
kosamad <>
2023-12-18 09:34:42 +0000

    Add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@
 // Get the button elements and add event listeners to them
 
 // Listens for DOM content to be loaded then executes the function
+
 document.addEventListener('DOMContentLoaded', function () {
 
     // return all buttons on the page as an array which we can iterate through
@@ -26,21 +27,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 // otherwise set game type to value of this attribute
 
                 let gameType = this.getAttribute('data-type');
-                runGame(gameType);;
+                runGame(gameType);
             }
         });
 
     }
 
-    // Allowing the user to press enter to submit an answer as well as click
-    //listening for a key press (the "event"). Calls a function with an the "event" object. Then check a property of that object, here 
-    // looking for if the key that was pressed was enter. if yes, then call the checkasnwer function. 
 
-    document.getElementById("answer-box").addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-            checkAnswer();
-        }
-    });
+    /*
+            // Allowing the user to press enter to submit an answer as well as click
+        //listening for a key press (the "event"). Calls a function with an the "event" object. Then check a property of that object, here 
+        // looking for if the key that was pressed was enter. if yes, then call the checkasnwer function. 
+    
+        document.getElementById("answer-box").addEventListener('keydown', function(event) {
+            if (event.key === 'Enter') {
+                checkAnswer();
+            }
+        });
+    
+    */
     // want this to start as soon as page is loaded (default game)
 
     runGame('addition');
@@ -67,6 +72,7 @@ function runGame(gameType) {
     // creates 2 random numbers betweem 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
     // check which game is running
 
     if (gameType === 'addition') {
@@ -75,7 +81,10 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
+        ;
         alert(`Unknown game type: ${gameType}`);
         // throw stops game running and prints to console to add debugging
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -137,6 +146,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "divsion"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -179,9 +190,6 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '+';
-
-
-
 }
 
 // two arguments that are accepted are operand1 and 2
@@ -200,3 +208,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operator').textContent = "x";
 
 }
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = (operand1 * operand2);
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+
+}
+
